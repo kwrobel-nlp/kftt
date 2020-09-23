f807e757023fb8f2fc6e9fcf5044a6ec9aa874c4
Krzysztof Wróbel <djstrong@gmail.com>
2020-09-23 13:27:54 +0200

    show duplicates

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,3 +1,4 @@
+import sys
 from argparse import ArgumentParser
 
 import jsonlines
@@ -17,6 +18,12 @@ with jsonlines.open(args.input_path) as reader:
         # text=ktext.infer_original_text()
         if text not in reference_paragraphs:
             reference_paragraphs[text] = ktext
+        else:
+            print('DUPLICATE', file=sys.stderr)
+            print(text, file=sys.stderr)
+            print(ktext.repr(), file=sys.stderr)
+            print(reference_paragraphs[text].repr(), file=sys.stderr)
+            print(file=sys.stderr)
 
 with jsonlines.open(args.output_path, 'w') as writer:
     for ktext in reference_paragraphs.values():
